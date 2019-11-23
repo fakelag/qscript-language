c0e335f39e6000efbb3e927f79276827825add46
fakelag <35497506+fakelag@users.noreply.github.com>
2019-11-23 14:09:58 +0200

    Add initial AST nodes

--- Library/Compiler/AST.h
+++ Library/Compiler/AST.h
@@ -1,15 +1,53 @@
 namespace Compiler
 {
-	class IASTNode
+	enum NodeType
 	{
-	public:
-		virtual ~IASTNode();
-		virtual void Compile() = 0;
+		NT_INVALID = -1,
+		NT_TERM,			// Terminating node, links to no further nodes
+		NT_VALUE,			// Value node. Contains a value, links to no further nodes
+		// NT_SIMPLE,			// Simple node, links to a single subtree (of 1...n items)
 	};
 
-	class SimpleNode
+	enum NodeId
+	{
+		NODE_INVALID = -1,
+		NODE_CONSTANT,
+		NODE_RETURN,
+	};
+
+	class BaseNode
 	{
 	public:
-		void Compile();
+		BaseNode( int lineNr, int colNr, const std::string token, NodeType type, NodeId id );
+
+		NodeType Type()		const { return m_NodeType; }
+		NodeId Id()			const { return m_NodeId; }
+
+		virtual ~BaseNode() {}
+		virtual void Compile( QScript::Chunk_t* chunk ) = 0;
+
+	protected:
+		NodeId				m_NodeId;
+		NodeType			m_NodeType;
+		int					m_LineNr;
+		int					m_ColNr;
+		std::string 		m_Token;
+	};
+
+	class TermNode : public BaseNode
+	{
+	public:
+		TermNode( int lineNr, int colNr, const std::string token, NodeId id );
+		void Compile( QScript::Chunk_t* chunk );
+	};
+
+	class ValueNode : public BaseNode
+	{
+	public:
+		ValueNode( int lineNr, int colNr, const std::string token, NodeId id, const QScript::Value& value );
+		void Compile( QScript::Chunk_t* chunk );
+
+	private:
+		QScript::Value		m_Value;
 	};
 }
--- Library/Compiler/IRGenerator.cpp
+++ Library/Compiler/IRGenerator.cpp
@@ -1,10 +1,48 @@
-#include "../Includes/QLibPCH.h"
+#include "QLibPCH.h"
 #include "Compiler.h"
+#include "Instructions.h"
+#include "Exception.h"
 
 namespace Compiler
 {
-	IASTNode* GenerateIR( const std::vector< Token_t >& tokens )
+	std::vector< BaseNode* > GenerateIR( const std::vector< Token_t >& tokens )
 	{
-		return NULL;
+		auto constant = new ValueNode( 0, 0, "1.2", NODE_CONSTANT, 1.2 );
+		auto retn = new TermNode( 0, 0, "return", NODE_RETURN );
+		auto constant2 = new ValueNode( 0, 0, "1.55", NODE_CONSTANT, 1.55 );
+
+		return std::vector< BaseNode* >{ constant, constant2, retn };
+	}
+
+	BaseNode::BaseNode( int lineNr, int colNr, const std::string token, NodeType type, NodeId id )
+	{
+		m_LineNr		= lineNr;
+		m_ColNr			= colNr;
+		m_Token			= token;
+		m_NodeType		= type;
+		m_NodeId		= id;
+	}
+
+	TermNode::TermNode( int lineNr, int colNr, const std::string token, NodeId id )
+		: BaseNode( lineNr, colNr, token, NT_TERM, id )
+	{
+	}
+
+	void TermNode::Compile( QScript::Chunk_t* chunk )
+	{
+		// Call CompileXXX functions from Compiler.cpp for code generation,
+		// but append debugging info to the chunk here
+		CompileTermNode( m_NodeId, chunk );
+	}
+
+	ValueNode::ValueNode( int lineNr, int colNr, const std::string token, NodeId id, const QScript::Value& value )
+		: BaseNode( lineNr, colNr, token, NT_VALUE, id )
+	{
+		m_Value = value;
+	}
+
+	void ValueNode::Compile( QScript::Chunk_t* chunk )
+	{
+		CompileValueNode( m_Value, chunk );
 	}
 }
