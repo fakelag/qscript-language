f38800e250272479277afb7d87cc30d42de2ec4c
fakelag <35497506+fakelag@users.noreply.github.com>
2019-11-25 19:29:25 +0200

    Move AST objects code to AST.cpp

--- Library/Compiler/IRGenerator.cpp
+++ Library/Compiler/IRGenerator.cpp
@@ -64,36 +64,4 @@ namespace Compiler
 		parserState.AddNode( new TermNode( 0, 0, "", NODE_RETURN ) );
 		return parserState.Product();
 	}
-
-	BaseNode::BaseNode( int lineNr, int colNr, const std::string token, NodeType type, NodeId id )
-	{
-		m_LineNr		= lineNr;
-		m_ColNr			= colNr;
-		m_Token			= token;
-		m_NodeType		= type;
-		m_NodeId		= id;
-	}
-
-	TermNode::TermNode( int lineNr, int colNr, const std::string token, NodeId id )
-		: BaseNode( lineNr, colNr, token, NT_TERM, id )
-	{
-	}
-
-	void TermNode::Compile( QScript::Chunk_t* chunk )
-	{
-		// Call CompileXXX functions from Compiler.cpp for code generation,
-		// but append debugging info to the chunk here
-		CompileTermNode( m_NodeId, chunk );
-	}
-
-	ValueNode::ValueNode( int lineNr, int colNr, const std::string token, NodeId id, const QScript::Value& value )
-		: BaseNode( lineNr, colNr, token, NT_VALUE, id )
-	{
-		m_Value = value;
-	}
-
-	void ValueNode::Compile( QScript::Chunk_t* chunk )
-	{
-		CompileValueNode( m_Value, chunk );
-	}
 }
