7279be9ea49cbe1a22a0d0cbb548c777c6860f66
fakelag <35497506+fakelag@users.noreply.github.com>
2019-11-27 21:20:34 +0200

    Test compiling multiple consequent additions

--- CLI/CLI.cpp
+++ CLI/CLI.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	QScript::Chunk_t chunk = QScript::Compile( "1.5 + 5.5;" );
+	QScript::Chunk_t chunk = QScript::Compile( "1.5 + 5.5 + 1 + 90.5;" );
 	QScript::Interpret( chunk );
 	return 0;
 }
