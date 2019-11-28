0098c3b9a1d42d36e20d04e4215c94976aba37bf
fakelag <35497506+fakelag@users.noreply.github.com>
2019-11-28 19:24:42 +0200

    Add some calculations into cli

--- CLI/CLI.cpp
+++ CLI/CLI.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	QScript::Chunk_t chunk = QScript::Compile( "1.5 + 5.5 + 1 + 90.5;" );
+	QScript::Chunk_t chunk = QScript::Compile( "(6.2 + 2) + 4 * 4 - 5 + 3 / 6 * 2 + ((4 + 6.5) - 2.2);" );
 	QScript::Interpret( chunk );
 	return 0;
 }
