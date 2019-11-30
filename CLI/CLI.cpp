2f63e72e30fb0d80b211b4d9d7fc3670320d8e84
fakelag <35497506+fakelag@users.noreply.github.com>
2019-11-30 19:09:06 +0200

    Add CLI exception handling

--- CLI/CLI.cpp
+++ CLI/CLI.cpp
@@ -1,8 +1,30 @@
+#include <iostream>
 #include "../Includes/QScript.h"
+#include "../Includes/Exception.h"
 
 int main()
 {
-	QScript::Chunk_t chunk = QScript::Compile( "-8.2 - (6.0 + 2) + 4.5 * 4.2 - 5.2 + (-3 / 6) * 2 + ((4 + 6.0) - 2.0);" );
-	QScript::Interpret( chunk );
+	try
+	{
+		QScript::Chunk_t chunk = QScript::Compile( "-false;" ); // "-8.2 - (6.0 + 2) + 4.5 * 4.2 - 5.2 + (-3 / 6) * 2 + ((4 + 6.0) - 2.0);" );
+		QScript::Interpret( chunk );
+	}
+	catch ( const RuntimeException& exception )
+	{
+		std::cout << "Exception (" << exception.id() << "): " << exception.what() << std::endl;
+	}
+	catch ( const Exception& exception )
+	{
+		std::cout << "Exception (" << exception.id() << "): " << exception.what() << std::endl;
+	}
+	catch ( const std::exception& exception )
+	{
+		std::cout << "Exception: " << exception.what() << std::endl;
+	}
+	catch ( ... )
+	{
+		std::cout << "Unknown exception occurred." << std::endl;
+	}
+
 	return 0;
 }
