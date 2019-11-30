2963a705289c5529a93a2f9d9fe1d7b04e0e61e5
fakelag <35497506+fakelag@users.noreply.github.com>
2019-11-30 19:07:50 +0200

    Add RuntimeException type

--- Includes/Exception.h
+++ Includes/Exception.h
@@ -13,8 +13,26 @@ public:
 		m_What = description;
 	}
 
+	const char* id() const _NOEXCEPT { return m_Id.c_str(); }
 	const char* what() const _NOEXCEPT { return m_What.c_str(); }
 protected:
 	std::string m_Id;
 	std::string m_What;
+};
+
+class RuntimeException : public Exception
+{
+public:
+	RuntimeException( const std::string& id, const std::string& description,
+		int lineNr, int colNr, const std::string& token )
+		: Exception( id, description )
+	{
+		m_LineNr 	= lineNr;
+		m_ColNr 	= colNr;
+		m_Token 	= token;
+	}
+protected:
+	int 			m_LineNr;
+	int 			m_ColNr;
+	std::string 	m_Token;
 };
