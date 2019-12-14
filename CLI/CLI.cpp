d4f3ae2873f940624bee0449a2e63dcf277dfecf
fakelag <35497506+fakelag@users.noreply.github.com>
2019-12-14 18:55:05 +0200

    Interactive repl

--- CLI/CLI.cpp
+++ CLI/CLI.cpp
@@ -1,32 +1,47 @@
-#include <iostream>
-#include <string>
+#include "QLibPCH.h"
 
 #include "../Includes/QScript.h"
 #include "../Includes/Exception.h"
 
 int main()
 {
-	try
+	std::string command;
+	for ( ;; )
 	{
-		QScript::Chunk_t* chunk = QScript::Compile( "\"hello \" + \"world!\";" );
-		QScript::Interpret( *chunk, NULL );
-		QScript::FreeChunk( chunk );
-	}
-	catch ( const RuntimeException& exception )
-	{
-		std::cout << "Exception (" << exception.id() << "): " << exception.what() << std::endl;
-	}
-	catch ( const Exception& exception )
-	{
-		std::cout << "Exception (" << exception.id() << "): " << exception.what() << std::endl;
-	}
-	catch ( const std::exception& exception )
-	{
-		std::cout << "Exception: " << exception.what() << std::endl;
-	}
-	catch ( ... )
-	{
-		std::cout << "Unknown exception occurred." << std::endl;
+		std::cout << "REPL > ";
+		std::getline( std::cin, command );
+
+		try
+		{
+			QScript::Chunk_t* chunk = QScript::Compile( command.c_str() );
+			QScript::Interpret( *chunk, NULL );
+			QScript::FreeChunk( chunk );
+		}
+		catch ( std::vector< CompilerException >& exceptions )
+		{
+			for ( auto ex : exceptions )
+				std::cout << ex.describe() << std::endl;
+		}
+		catch ( const RuntimeException& exception )
+		{
+			std::cout << "Exception (" << exception.id() << "): " << exception.what() << std::endl;
+		}
+		catch ( const Exception& exception )
+		{
+			std::cout << "Exception (" << exception.id() << "): " << exception.what() << std::endl;
+		}
+		catch ( const std::exception& exception )
+		{
+			std::cout << "Exception: " << exception.what() << std::endl;
+		}
+		catch ( ... )
+		{
+			std::cout << "Unknown exception occurred." << std::endl;
+		}
+
+		// Flush stdin
+		int ch;
+		while ( (ch = std::cin.get()) != '\n' && ch != EOF );
 	}
 
 	return 0;
