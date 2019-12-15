320ab2024ae3c88a2bfef2b5f05e53b6e2d7752d
fakelag <35497506+fakelag@users.noreply.github.com>
2019-12-15 18:03:50 +0200

    Fix converting double values to string

--- Library/Common/Value.h
+++ Library/Common/Value.h
@@ -114,7 +114,7 @@ namespace QScript
 			{
 				case VT_NUMBER:
 				{
-					char result[ 4 ];
+					char result[ 6 ];
 					snprintf( result, sizeof( result ), "%.2f", AS_NUMBER( *this ) );
 					return std::string( result );
 				}
