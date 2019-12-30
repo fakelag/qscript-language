41709b4872001f1eb4a6be3a81835d5faca35524
fakelag <35497506+fakelag@users.noreply.github.com>
2019-12-30 16:13:55 +0200

    Remove few unmaintainable compiler tests (replaced with fullstack tests)

--- Tests/TestCompiler.cpp
+++ Tests/TestCompiler.cpp
@@ -55,71 +55,16 @@ bool Tests::TestCompiler()
 		UTEST_CASE_CLOSED();
 	}( );
 
-	UTEST_CASE( "Compile > 255 constants" )
-	{
-		std::string sourceCode = TestUtils::GenerateSequence( 512, []( int iter ) {
-			return std::to_string( iter ) + std::string( ".00;" );
-		} );
-
-		auto chunk = QScript::Compile( sourceCode );
-
-		UTEST_ASSERT( chunk->m_Constants.size() == 512 );
-
-		ASSERT_OPCODE( 0000, OP_LOAD_CONSTANT_SHORT );
-		ASSERT_OPCODE_NEXT( OP_POP );
-
-		int firstLong = 256 * 3;
-		UTEST_ASSERT( chunk->m_Code[ firstLong ] == QScript::OpCode::OP_LOAD_CONSTANT_LONG );
-		UTEST_ASSERT( chunk->m_Code[ firstLong + 5 ] == QScript::OpCode::OP_POP );
-
-		opcodeOffset = 0;
-		QScript::FreeChunk( chunk );
-
-		chunk = QScript::Compile( sourceCode, QScript::OF_CONSTANT_STACKING );
-
-		UTEST_ASSERT( chunk->m_Constants.size() == 512 );
-
-		ASSERT_OPCODE_NEXT( OP_LOAD_CONSTANT_SHORT );
-		ASSERT_OPCODE_NEXT( OP_POP );
-
-		firstLong = 256 * 3;
-		UTEST_ASSERT( chunk->m_Code[ firstLong ] == QScript::OpCode::OP_LOAD_CONSTANT_LONG );
-		UTEST_ASSERT( chunk->m_Code[ firstLong + 5 ] == QScript::OpCode::OP_POP );
-
-		QScript::FreeChunk( chunk );
-		UTEST_CASE_CLOSED();
-	}( );
-
 	UTEST_CASE( "Constant stacking" )
 	{
 		auto chunk = QScript::Compile( TestUtils::GenerateSequence( 512, []( int iter ) {
-			return std::to_string( iter % 2 == 0 ? iter : 8000 ) + std::string( ".00;" );
-		} ), QScript::OF_CONSTANT_STACKING );
+			return "+" + std::to_string( iter % 2 == 0 ? iter : 8000 ) + std::string( ".00" );
+		}, "var f = 0.00 ", ";" ), QScript::OF_CONSTANT_STACKING );
 
-		UTEST_ASSERT( chunk->m_Constants.size() == 257 );
-
-		QScript::FreeChunk( chunk );
-
-		UTEST_CASE_CLOSED();
-	}( );
-
-	UTEST_CASE( "Assign constants to global variables" )
-	{
-		auto chunk = QScript::Compile( "var a; var b = 4; a = 32.2;" );
-
-		UTEST_ASSERT( chunk->m_Constants.size() == 5 );
-
-		ASSERT_OPCODE( 0000, OP_LOAD_NULL );
-		ASSERT_OPCODE_NEXT( OP_SET_GLOBAL_SHORT );
-		ASSERT_OPCODE_NEXT( OP_LOAD_CONSTANT_SHORT );
-		ASSERT_OPCODE_NEXT( OP_SET_GLOBAL_SHORT );
-
-		ASSERT_OPCODE_NEXT( OP_LOAD_CONSTANT_SHORT );
-		ASSERT_OPCODE_NEXT( OP_SET_GLOBAL_SHORT );
-
-		ASSERT_OPCODE_NEXT( OP_RETURN );
+		UTEST_ASSERT( chunk->m_Constants.size() == 258 );
 
 		QScript::FreeChunk( chunk );
+
 		UTEST_CASE_CLOSED();
 	}( );
 
