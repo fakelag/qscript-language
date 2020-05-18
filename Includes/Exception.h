ef077b537701de21862740e78a3d7c086fe7261c
fakelag <35497506+fakelag@users.noreply.github.com>
2020-05-18 21:14:43 +0300

    Add accessord to CompilerException

--- Includes/Exception.h
+++ Includes/Exception.h
@@ -39,6 +39,10 @@ public:
 			+ " (\"" + m_Token + "\")" );
 	}
 
+	int						LineNr()		{ return m_LineNr; }
+	int						ColNr()			{ return m_ColNr; }
+	const std::string&		Token()			{ return m_Token; }
+
 protected:
 	int 			m_LineNr;
 	int 			m_ColNr;
