48fc6162b56dd3852b17e9e3a3afb59f4a0bd2df
fakelag <35497506+fakelag@users.noreply.github.com>
2020-06-08 22:50:44 +0300

    Propagate module & disassembly data to vscode extension

--- LangSrv/LangSrv.cpp
+++ LangSrv/LangSrv.cpp
@@ -5,10 +5,13 @@
 
 #include "..\Library\Common\Chunk.h"
 #include "..\Library\Compiler\Compiler.h"
+#include "..\Library\Common\Disassembler.h"
+#include "..\Library\STL\NativeModule.h"
 
 bool g_ListSymbols = false;
 bool g_FirstSymbol = true;
 std::string g_SymbolList;
+// std::unordered_map< uintptr_t, int > g_Disassemblies; // WIP
 
 bool GetArg( const std::string& argument, int argc, char* argv[], std::string* next )
 {
@@ -26,6 +29,29 @@ bool GetArg( const std::string& argument, int argc, char* argv[], std::string* n
 	}
 
 	return false;
+}
+
+std::string ReadFile( const std::string& path )
+{
+	std::string content = "";
+	std::ifstream programFile;
+
+	programFile.open( path );
+
+	if ( programFile.is_open() )
+	{
+		std::string line;
+		while ( std::getline( programFile, line ) )
+			content += line + "\n";
+
+		programFile.close();
+	}
+	else
+	{
+		throw Exception( "langsrv_no_file_found", "File \"" + path + "\" was not found" );
+	}
+
+	return content;
 }
 
 std::string JsonEscape( const std::string& input )
@@ -65,7 +91,12 @@ int main( int argc, char* argv[] )
 #endif
 
 	std::string next;
+	std::string input = "";
+
 	g_ListSymbols = GetArg( "--listSymbols", argc, argv, &next );
+
+	if ( GetArg( "--file", argc, argv, &next ) )
+		input = ReadFile( next );
 
 	for ( ;;)
 	{
@@ -74,8 +105,15 @@ int main( int argc, char* argv[] )
 			std::string source;
 			std::string newLine;
 
-			while ( std::getline( std::cin, newLine ) )
-				source += newLine + "\n";
+			if ( input.length() == 0 )
+			{
+				while ( std::getline( std::cin, newLine ) )
+					source += newLine + "\n";
+			}
+			else
+			{
+				source = input;
+			}
 
 			QScript::Config_t config( true );
 
@@ -89,14 +127,100 @@ int main( int argc, char* argv[] )
 				if ( !g_FirstSymbol )
 					g_SymbolList += ",";
 
+				// Regular symbol
 				g_SymbolList += "{ \"context\": \"" + context + "\""
 					+ ", \"lineNr\": " + std::to_string( lineNr )
 					+ ", \"colNr\": " + std::to_string( colNr )
 					+ ", \"name\": \"" + JsonEscape( variable.m_Name ) + "\""
 					+ ", \"isConst\": " + ( variable.m_IsConst ? "true" : "false" )
 					+ ", \"type\": \"" + JsonEscape( type ) + "\""
-					+ ", \"returnType\": \"" + JsonEscape( returnType ) + "\""
-					+ "}";
+					+ ", \"returnType\": \"" + JsonEscape( returnType ) + "\"";
+
+				if ( variable.m_IsConst && variable.m_Function )
+				{
+					// Function constant, include disassembly
+					auto chunk = variable.m_Function->GetChunk();
+
+					auto disasm = Disassembler::DisassembleChunk( *chunk, variable.m_Function->GetName() );
+
+					g_SymbolList += ", \"disassembly\": { \"name\": \"" + disasm.m_Name + "\", \"opcodes\": [";
+
+					for ( size_t i = 0; i < disasm.m_Opcodes.size(); ++i )
+					{
+						auto opcode = disasm.m_Opcodes[ i ];
+
+						std::string opCodeType = "SIMPLE";
+						switch ( opcode.m_Type )
+						{
+						case Disassembler::OPCODE_LONG: opCodeType = "LONG"; break;
+						case Disassembler::OPCODE_SHORT: opCodeType = "SHORT"; break;
+						default: break;
+						}
+
+						if ( i > 0 ) g_SymbolList += ",";
+
+						g_SymbolList += "{ \"full\": \"" + opcode.m_Full + "\""
+							+ ", \"address\": \"" + std::to_string( opcode.m_Address ) + "\""
+							+ ", \"lineNr\": " + std::to_string( opcode.m_LineNr )
+							+ ", \"colNr\": " + std::to_string( opcode.m_ColNr )
+							+ ", \"token\": \"" + JsonEscape( opcode.m_Token ) + "\""
+							+ ", \"type\": \"" + opCodeType + "\""
+							+ ", \"size\": \"" + std::to_string( opcode.m_Size ) + "\""
+							+ "}";
+					}
+
+					g_SymbolList += "]}";
+				}
+
+				g_SymbolList += "}";
+
+				g_FirstSymbol = false;
+			};
+
+			config.m_ImportCb = []( int lineNr, int colNr, const std::string& moduleName, const std::vector< QScript::NativeFunctionSpec_t >& functions ) -> void {
+				if ( !g_ListSymbols )
+					return;
+
+				if ( !g_FirstSymbol )
+					g_SymbolList += ",";
+
+				g_SymbolList += std::string( "{ \"context\": \"Import\"" )
+					+ ", \"lineNr\": " + std::to_string( lineNr )
+					+ ", \"colNr\": " + std::to_string( colNr )
+					+ ", \"name\": \"" + JsonEscape( moduleName ) + "\""
+					+ ", \"isConst\": true"
+					+ ", \"type\": \"module\""
+					+ ", \"moduleFunctions\": [";
+
+				for ( size_t i = 0; i < functions.size(); ++i )
+				{
+					auto& func = functions[ i ];
+					std::string returnType = Compiler::TypeToString( func.m_ReturnTypeBits );
+
+					g_SymbolList += std::string( "{" )
+						+ "\"name\": \"" + JsonEscape( func.m_Name ) + "\","
+						+ "\"returnType\": \"" + JsonEscape( returnType ) + "\","
+						+ "\"args\": [";
+
+					for ( size_t j = 0; j < func.m_Args.size(); ++j )
+					{
+						auto& arg = func.m_Args[ j ];
+
+						std::string argType = Compiler::TypeToString( arg.m_TypeBits );
+						std::string argReturnType = Compiler::TypeToString( arg.m_ReturnTypeBits );
+
+						g_SymbolList += std::string( "{" )
+							+ "\"name\": \"" + JsonEscape( arg.m_ArgName ) + "\","
+							+ "\"type\": \"" + JsonEscape( argType ) + "\","
+							+ "\"returnType\": \"" + JsonEscape( argReturnType ) + "\","
+							+ "\"isVarArgs\": " + ( arg.m_IsVarArgs ? "true" : "false" )
+							+ "}" + ( j < func.m_Args.size() - 1 ? "," : "" );
+					}
+
+					g_SymbolList += std::string( "]}" ) + ( i < functions.size() - 1 ? "," : "" );
+				}
+
+				g_SymbolList += "]}";
 
 				g_FirstSymbol = false;
 			};
@@ -136,15 +260,18 @@ int main( int argc, char* argv[] )
 		}
 		catch ( const Exception& exception )
 		{
-			std::cout << "[" + exception.id() + "]:" + exception.describe() << std::endl;
+			std::cout << "{ \"status\": \"failed\", \"errors\": [";
+			std::cout << "{ \"id\": \"" + exception.id() + "\", \"desc\": \"" + exception.describe() + "\", \"generic\": true }] }" << std::endl;
 		}
 		catch ( const std::exception& exception )
 		{
-			std::cout << std::string( "[std_generic]:" ) + exception.what() << std::endl;
+			std::cout << "{ \"status\": \"failed\", \"errors\": [";
+			std::cout << "{ \"id\": \"unknown\", \"desc\": \"" + std::string( exception.what() ) + "\", \"generic\": true }] }" << std::endl;
 		}
 		catch ( ... )
 		{
-			std::cout << "[unknown]" << std::endl;
+			std::cout << "{ \"status\": \"failed\", \"errors\": [";
+			std::cout << "{ \"id\": \"unknown\", \"generic\": true }] }" << std::endl;
 		}
 
 		if ( GetArg( "--exit", argc, argv, &next ) )
