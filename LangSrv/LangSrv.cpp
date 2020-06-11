b2144d13b6f007780d2d516b66102d5f417450d1
fakelag <35497506+fakelag@users.noreply.github.com>
2020-06-11 19:56:50 +0300

    Compile disassemblies to a list rather than inplace in langsrv

--- LangSrv/LangSrv.cpp
+++ LangSrv/LangSrv.cpp
@@ -11,7 +11,8 @@
 bool g_ListSymbols = false;
 bool g_FirstSymbol = true;
 std::string g_SymbolList;
-// std::unordered_map< uintptr_t, int > g_Disassemblies; // WIP
+std::string g_DisasmList;
+std::unordered_map< const QScript::Chunk_t*, int > g_Disassemblies;
 
 bool GetArg( const std::string& argument, int argc, char* argv[], std::string* next )
 {
@@ -84,6 +85,49 @@ std::string JsonEscape( const std::string& input )
 	return o.str();
 }
 
+int AddDisassembly( const QScript::Chunk_t* chunk, const std::string& name )
+{
+	auto disasm = Disassembler::DisassembleChunk( *chunk, name );
+
+	if ( g_DisasmList.length() == 0 )
+		g_DisasmList = "{";
+	else
+		g_DisasmList += ",{";
+
+	g_DisasmList += "\"name\":\"" + disasm.m_Name + "\",\"opcodes\":[";
+
+	for ( size_t i = 0; i < disasm.m_Opcodes.size(); ++i )
+	{
+		auto opcode = disasm.m_Opcodes[ i ];
+
+		std::string opCodeType = "SIMPLE";
+		switch ( opcode.m_Type )
+		{
+		case Disassembler::OPCODE_LONG: opCodeType = "LONG"; break;
+		case Disassembler::OPCODE_SHORT: opCodeType = "SHORT"; break;
+		default: break;
+		}
+
+		if ( i > 0 ) g_DisasmList += ",";
+
+		g_DisasmList += "{\"full\":\"" + opcode.m_Full + "\""
+			+ ",\"address\":\"" + std::to_string( opcode.m_Address ) + "\""
+			+ ",\"lineNr\":" + std::to_string( opcode.m_LineNr )
+			+ ",\"colNr\":" + std::to_string( opcode.m_ColNr )
+			+ ",\"token\":\"" + JsonEscape( opcode.m_Token ) + "\""
+			+ ",\"type\":\"" + opCodeType + "\""
+			+ ",\"size\":\"" + std::to_string( opcode.m_Size ) + "\""
+			+ "}";
+	}
+
+	int disasmId = g_Disassemblies.size() + 1;
+
+	g_DisasmList += "], \"id\": " + std::to_string( disasmId ) + "}";
+
+	g_Disassemblies[ chunk ] = disasmId;
+	return disasmId;
+}
+
 int main( int argc, char* argv[] )
 {
 #ifdef QS_MEMLEAK_TEST
@@ -128,48 +172,29 @@ int main( int argc, char* argv[] )
 					g_SymbolList += ",";
 
 				// Regular symbol
-				g_SymbolList += "{ \"context\": \"" + context + "\""
-					+ ", \"lineNr\": " + std::to_string( lineNr )
-					+ ", \"colNr\": " + std::to_string( colNr )
-					+ ", \"name\": \"" + JsonEscape( variable.m_Name ) + "\""
-					+ ", \"isConst\": " + ( variable.m_IsConst ? "true" : "false" )
-					+ ", \"type\": \"" + JsonEscape( type ) + "\""
-					+ ", \"returnType\": \"" + JsonEscape( returnType ) + "\"";
+				g_SymbolList += "{\"context\":\"" + context + "\""
+					+ ",\"lineNr\":" + std::to_string( lineNr )
+					+ ",\"colNr\":" + std::to_string( colNr )
+					+ ",\"name\":\"" + JsonEscape( variable.m_Name ) + "\""
+					+ ",\"isConst\": " + ( variable.m_IsConst ? "true" : "false" )
+					+ ",\"type\":\"" + JsonEscape( type ) + "\""
+					+ ",\"returnType\":\"" + JsonEscape( returnType ) + "\"";
 
 				if ( variable.m_IsConst && variable.m_Function )
 				{
 					// Function constant, include disassembly
 					auto chunk = variable.m_Function->GetChunk();
 
-					auto disasm = Disassembler::DisassembleChunk( *chunk, variable.m_Function->GetName() );
+					auto disassembly = g_Disassemblies.find( chunk );
 
-					g_SymbolList += ", \"disassembly\": { \"name\": \"" + disasm.m_Name + "\", \"opcodes\": [";
+					int disasmId = 0;
+					if ( disassembly == g_Disassemblies.end() )
+						disasmId = AddDisassembly( chunk, variable.m_Function->GetName() );
+					else
+						disasmId = disassembly->second;
 
-					for ( size_t i = 0; i < disasm.m_Opcodes.size(); ++i )
-					{
-						auto opcode = disasm.m_Opcodes[ i ];
-
-						std::string opCodeType = "SIMPLE";
-						switch ( opcode.m_Type )
-						{
-						case Disassembler::OPCODE_LONG: opCodeType = "LONG"; break;
-						case Disassembler::OPCODE_SHORT: opCodeType = "SHORT"; break;
-						default: break;
-						}
-
-						if ( i > 0 ) g_SymbolList += ",";
-
-						g_SymbolList += "{ \"full\": \"" + opcode.m_Full + "\""
-							+ ", \"address\": \"" + std::to_string( opcode.m_Address ) + "\""
-							+ ", \"lineNr\": " + std::to_string( opcode.m_LineNr )
-							+ ", \"colNr\": " + std::to_string( opcode.m_ColNr )
-							+ ", \"token\": \"" + JsonEscape( opcode.m_Token ) + "\""
-							+ ", \"type\": \"" + opCodeType + "\""
-							+ ", \"size\": \"" + std::to_string( opcode.m_Size ) + "\""
-							+ "}";
-					}
-
-					g_SymbolList += "]}";
+					
+					g_SymbolList += ",\"disassemblyId\":" + std::to_string( disasmId );
 				}
 
 				g_SymbolList += "}";
@@ -184,13 +209,13 @@ int main( int argc, char* argv[] )
 				if ( !g_FirstSymbol )
 					g_SymbolList += ",";
 
-				g_SymbolList += std::string( "{ \"context\": \"Import\"" )
-					+ ", \"lineNr\": " + std::to_string( lineNr )
-					+ ", \"colNr\": " + std::to_string( colNr )
-					+ ", \"name\": \"" + JsonEscape( moduleName ) + "\""
-					+ ", \"isConst\": true"
-					+ ", \"type\": \"module\""
-					+ ", \"moduleFunctions\": [";
+				g_SymbolList += std::string( "{\"context\":\"Import\"" )
+					+ ",\"lineNr\":" + std::to_string( lineNr )
+					+ ",\"colNr\":" + std::to_string( colNr )
+					+ ",\"name\": \"" + JsonEscape( moduleName ) + "\""
+					+ ",\"isConst\": true"
+					+ ",\"type\":\"module\""
+					+ ",\"moduleFunctions\":[";
 
 				for ( size_t i = 0; i < functions.size(); ++i )
 				{
@@ -198,9 +223,9 @@ int main( int argc, char* argv[] )
 					std::string returnType = Compiler::TypeToString( func.m_ReturnTypeBits );
 
 					g_SymbolList += std::string( "{" )
-						+ "\"name\": \"" + JsonEscape( func.m_Name ) + "\","
-						+ "\"returnType\": \"" + JsonEscape( returnType ) + "\","
-						+ "\"args\": [";
+						+ "\"name\":\"" + JsonEscape( func.m_Name ) + "\","
+						+ "\"returnType\":\"" + JsonEscape( returnType ) + "\","
+						+ "\"args\":[";
 
 					for ( size_t j = 0; j < func.m_Args.size(); ++j )
 					{
@@ -210,10 +235,10 @@ int main( int argc, char* argv[] )
 						std::string argReturnType = Compiler::TypeToString( arg.m_ReturnTypeBits );
 
 						g_SymbolList += std::string( "{" )
-							+ "\"name\": \"" + JsonEscape( arg.m_ArgName ) + "\","
-							+ "\"type\": \"" + JsonEscape( argType ) + "\","
-							+ "\"returnType\": \"" + JsonEscape( argReturnType ) + "\","
-							+ "\"isVarArgs\": " + ( arg.m_IsVarArgs ? "true" : "false" )
+							+ "\"name\":\"" + JsonEscape( arg.m_ArgName ) + "\","
+							+ "\"type\":\"" + JsonEscape( argType ) + "\","
+							+ "\"returnType\":\"" + JsonEscape( argReturnType ) + "\","
+							+ "\"isVarArgs\":" + ( arg.m_IsVarArgs ? "true" : "false" )
 							+ "}" + ( j < func.m_Args.size() - 1 ? "," : "" );
 					}
 
@@ -221,15 +246,16 @@ int main( int argc, char* argv[] )
 				}
 
 				g_SymbolList += "]}";
-
 				g_FirstSymbol = false;
 			};
 
 			auto mainFunction = QScript::Compile( source, config );
 
 			std::cout <<
-				"{ \"status\": \"OK\", \"errors\": [], \"symbols\": ["
+				"{\"status\":\"OK\",\"errors\":[],\"symbols\":["
 				+ g_SymbolList
+				+ "],\"disasm\":["
+				+ g_DisasmList
 				+ "]}" << std::endl;
 
 			QScript::FreeFunction( mainFunction );
@@ -242,8 +268,10 @@ int main( int argc, char* argv[] )
 			{
 				auto& ex = exceptions[ i ];
 
-				auto description = ( ex.What() + std::string( " on line " )
-					+ std::to_string( ex.LineNr() ) + " character " + std::to_string( ex.ColNr() ) );
+				auto description = ( ex.What() + std::string( " (line " )
+					+ std::to_string( ex.LineNr() )
+					+ " character " + std::to_string( ex.ColNr() )
+					+ " token \"" + ex.Token() + "\")" );
 
 				std::cout << std::string( "{" )
 					+ "\"lineNr\": " + std::to_string( ex.LineNr() ) + ","
